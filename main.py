d9920d994411c6170d2912d668a9284818a856b3
Martin deeagle Kock <mko@leibniz-psychology.org>
2024-06-28 12:07:46 +0200

    refactor: move demo api call in own method

--- main.py
+++ main.py
@@ -14,10 +14,12 @@ def print_statistics(api_endpoint: dspyce.rest.RestAPI, human_readable=True) -> 
         stats = json.dumps(stats, indent=2)
     print(stats)
 
-
-if __name__ == "__main__":
+def access_demo_api_and_print_statistics() -> None:
     print("Demo implementation of a dspyce library")
     url = "https://demo.dspace.org/server/api"
-    apiEndpoint = get_rest_api_connection(url)
-    print_statistics(apiEndpoint)
+    api_endpoint = get_rest_api_connection(url)
+    print_statistics(api_endpoint)
     print("Demo successfully finished")
+
+if __name__ == "__main__":
+    access_demo_api_and_print_statistics()
